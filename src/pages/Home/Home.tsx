839c24c83eeb1b245f5feec80c26d2f8679ff08f
Shonakar <carlosjbordon@gmail.com>
2023-01-23 18:08:31 -0300

    home: typing animation

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import Typed from 'react-typed';
 
 const Home = () => {
 	return (
@@ -7,7 +7,15 @@ const Home = () => {
 				<h1>Hola a todos, soy Yona!</h1>
 				<p>O asi me dicen :)</p>
 			</div>
-			<h3>Desarrollador Web: Front-End, Back End en proceso... </h3>
+			<h3>
+				Desarrollador Web:{' '}
+				<Typed
+					strings={['Front-End', 'Back End (en proceso...)']}
+					typeSpeed={150}
+					backSpeed={100}
+					loop
+				/>
+			</h3>
 			<div>linkedin y git botones</div>
 			<p>Navega por la pagina para saber mas sobre mi.</p>
 		</div>
