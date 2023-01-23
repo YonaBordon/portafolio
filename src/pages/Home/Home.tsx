a71626cf638c0b79eac3c1f1ee227a4191154ba8
Shonakar <carlosjbordon@gmail.com>
2023-01-23 20:38:40 -0300

    home: style

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,17 +6,10 @@ const Home = () => {
 		<div className='home-container'>
 			<div>
 				<h1>Hola a todos, soy Yona!</h1>
-				<p>O asi me dicen :)</p>
+				<p>Asi me dicen :)</p>
 			</div>
 			<h3>
-				Desarrollador Web:{' '}
-				<Typed
-					strings={['Front-End', 'Back End (en proceso...)']}
-					typeSpeed={150}
-					backSpeed={100}
-					smartBackspace
-					loop
-				/>
+				<Typed strings={['Desarrollador Front-End de Santa Fe, Argentina.']} typeSpeed={50} />
 			</h3>
 			<SocialMedia />
 			<p>Navega por la pagina para saber mas sobre mi.</p>
